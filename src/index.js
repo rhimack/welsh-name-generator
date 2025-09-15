6569873d0838108850130b111dff6413a0c02414
rhimack <remackness@outlook.com>
2025-09-15 19:11:55 +0100

    JS added with AI elements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-function generateName(event) {
-  event.preventDefault();
-
-  let nameElement = document.querySelector("#name");
-
+function displayName(response) {
+  console.log("Name generated");
   new Typewriter("#name", {
-    strings: "name...",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
+function generateName(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "3b3935e5a0ob7af4fft24a1b0a07901a";
+  let prompt =
+    "User instructions: Generate a traditional welsh name that begins with ${instructionsInput.value}";
+  let context =
+    "You are a welsh name expert and your mission is to provide a welsh name begining with any letter stated in basic HTML. Please make sure to follow the user instructions. Not include sogn title. Sign the poes 'by SheCodes AI'inside a <strong> element.";
+  let apiURL =
+    "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}";
+
+  console.log("Generating name");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayName);
+}
+
 let nameFormElement = document.querySelector("#name-form");
-nameFormElement.addEventListener("submit".generateName);
+nameFormElement.addEventListener("submit", generateName);
